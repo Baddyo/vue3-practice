330d3a1d77075eed29422978fb186285cb4f4933
肖遥 <xiaoyao@xiaoyaodeMacBook-Pro.local>
2020-10-09 08:52:08 +0800

    读完高阶部分

--- one/src/App.vue
+++ one/src/App.vue
@@ -1,24 +1,33 @@
 <template>
-
+  <compo nm="Dogg"></compo>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
-    
+  import {
+    provide,
+    reactive,
+    ref
+  } from 'vue';
+  import Compo from "./components/compo";
+  export default {
+    name: 'App',
+    components: {
+      Compo
+    },
+    setup() {
+      let a = 100;
+      let b = ref('badd');
+      let c = reactive(true);
+      provide('a', a);
+      provide('b', b);
+      provide('c', c);
+      a++;
+      b.value = 'bigg';
+      c = false;
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ one/src/components/compo.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>It's a COMPO</h2>
+    <slot></slot>
+    <slot name="part"></slot>
+    <p>{{a}}</p>
+    <p>{{b}}</p>
+    <p>{{c}}</p>
+</template>
+
+<script>
+import { inject, toRefs } from 'vue';
+    export default {
+        name: 'compo',
+        props: {
+            nm: {
+                type: String,
+                default: 'Badd'
+            }
+        },
+        setup(props, context) {
+            // let {nm1} = props; // 解构出来的 prop 没有响应性
+
+            // let {nm} = toRefs(props);
+            // nm.value = 'Swing'; // toRefs 处理后的 prop 是只读的
+            console.log('context', context.slots);
+
+            let a = inject('a');
+            let b = inject('b');
+            let c = inject('c');
+
+            return {a, b, c};
+        }
+    }
+</script>
+
+<style>
+
+</style>
